ae2fc7ef0a5c8f181721db3e4dff07323a72aaa7
ingequiroga1 <ingequiroga1@gmail.com>
2023-07-14 13:20:05 -0600

    Se corrige error en bd

--- api/config/database.php
+++ api/config/database.php
@@ -2,7 +2,7 @@
 class Database{
  
     // specify your own database credentials
-    private $host = "localhost";
+    private $host = "172.17.0.4";
     private $db_name = "inmuebles";
     //private $db_name = "inmueblests";
     private $username = "root";
